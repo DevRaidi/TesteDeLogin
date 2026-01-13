e3d04c23f8ffe000bfc15f948260d3fdad80b759
Ridlaw <fernandes.raidi@gmail.com>
2026-01-12 22:06:32 -0300

    Validação de cadastro

--- src/TesteDeLogin.java
+++ src/TesteDeLogin.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class TesteDeLogin{
-    public static void main(String[] arg){
+    public static void main(String[] args){
         Scanner leitor = new Scanner(System.in);
         System.out.println("Vamos realizar o seu cadastro");
         String cadastroUsuario;
@@ -20,12 +20,26 @@ public class TesteDeLogin{
         while(true){
             System.out.println("Cadastre uma senha com 6 números:");
             senha = leitor.nextLine();
-            if(senha.matches("\\d{6}+")){
+            if(senha.matches("\\d{6}")){
                 System.out.println("Cadastro de senha realizado com sucesso.");
                 break;
             } else {
                 System.out.println("Cadastro de senha inválido! A senha deve ter 6 números.");
             }
         }
+        // Login
+        System.out.println("\n===LOGIN===");
+        System.out.println("Digite o seu usuário: ");
+        String usuarioLogin = leitor.nextLine();
+
+        System.out.println("Digite sua senha: ");
+        String senhaLogin = leitor.nextLine();
+
+            if(usuarioLogin.equals(cadastroUsuario) && senhaLogin.equals(senha)){
+                System.out.println("Login realizado com sucesso!");
+            } else {
+                System.out.println("Usuário ou senha inválidos. Por favor verifique os dados do seu login");
+            }
+            leitor.close();
     }
 }
